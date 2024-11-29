6d05e8d81bef031cfff4bf171b28c3bfe02a464b
Notoriousjayy <38364814+Notoriousjayy@users.noreply.github.com>
2024-11-29 14:49:09 -0500

    Update babel.config.mjs

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,6 +1,6 @@
-export default {
+module.exports = {
   presets: [
-    "@babel/preset-env",
+    ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-react",
     "@babel/preset-typescript"
   ]
